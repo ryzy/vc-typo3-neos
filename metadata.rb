133997fbbecad6ee6b0926bd5c7835cbe6f954e5
ryzy <marcin@ryzycki.com>
2014-02-02 19:08:32 +0000

    Renamed to typo3-neos. Initial server setup moved to lemp-server cookbook
    Version bumped to 2.0 due to breaking changes.

--- metadata.rb
+++ metadata.rb
@@ -1,18 +1,12 @@
-name             'vc-typo3'
-maintainer       'ryzy'
+name             'typo3-neos'
+maintainer       'Marcin Ryzycki'
 maintainer_email 'marcin@ryzycki.com'
-license          'All rights reserved'
-description      'Installs/Configures vc-typo3'
+license          'GPL 2'
+description      'Installs/configures TYPO3 Neos environment'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '2.0.0'
 
-depends 'swap'            # https://github.com/sethvargo-cookbooks/swap
-depends 'yum'             # https://github.com/opscode-cookbooks/yum
-depends 'mysql'           # https://github.com/opscode-cookbooks/mysql
+depends 'lemp-server'     # https://github.com/ryzy/vc-lemp-server
 depends 'database'        # https://github.com/opscode-cookbooks/database
 depends 'nginx'           # https://github.com/opscode-cookbooks/nginx
-depends 'php', '~> 1.2.0' # https://github.com/opscode-cookbooks/php
-depends 'php-fpm'         # https://github.com/yevgenko/cookbook-php-fpm
 depends 'composer'        # https://github.com/escapestudios/chef-composer
-depends 'nfs'             # https://github.com/atomic-penguin/cookbook-nfs
-depends 'rvm'             # https://github.com/fnichol/chef-rvm
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,16 +1,8 @@
 #
-# Cookbook Name:: vc-typo3
+# Cookbook Name:: typo3-neos
 # Recipe:: default
 #
 # Copyright (C) 2014 ryzy
 #
 
-include_recipe 'vc-typo3::system'           # system basic setup (repos, tweaks)
-include_recipe 'vc-typo3::nfs'              # NFS exports
-include_recipe 'vc-typo3::web-tools'        # Web tools: ruby, sass, compass etc
-include_recipe 'vc-typo3::web-db'           # Web: MySQL
-include_recipe 'vc-typo3::web-nginx'        # Web: Nginx server
-include_recipe 'vc-typo3::web-php'          # Web: PHP, PHP-FPM, composer, phpMyAdmin
-include_recipe 'vc-typo3::typo3-flow'       # TYPO3 Flow installation
-include_recipe 'vc-typo3::typo3-neos'       # TYPO3 Neos installation
-include_recipe 'vc-typo3::user'             # user preferences etc
+include_recipe 'typo3-neos::neos'
