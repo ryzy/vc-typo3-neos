db578b81da78bd67089e50090f38dbdcb3555045
ryzy <marcin@ryzycki.com>
2014-02-03 17:06:16 +0000

    Fix for neos vhost: FLOW_CONTEXT was not set properly

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'marcin@ryzycki.com'
 license          'GPL 2'
 description      'Installs/configures TYPO3 Neos environment'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.0'
+version          '2.0.1'
 
 depends 'lemp-server'     # https://github.com/ryzy/vc-lemp-server
 depends 'database'        # https://github.com/opscode-cookbooks/database
